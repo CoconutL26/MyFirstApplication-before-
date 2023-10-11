d0a627f0c58baa0a1aef31ebf2e7456be4ab2f5c
CoconutL26 <2460045654@qq.com>
2023-10-11 22:28:56 +0800

    移动开发实验二

--- app/src/main/java/com/jnu/student/MainActivity.java
+++ app/src/main/java/com/jnu/student/MainActivity.java
@@ -1,7 +1,6 @@
 package com.jnu.student;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,11 +8,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = findViewById(R.id.text_vciew_hellow_world);
-        textView.setText(getString(R.string.hello_world));
+        //String helloWorld = getResources().getString(R.string.hello_world);
     }
 
 }
