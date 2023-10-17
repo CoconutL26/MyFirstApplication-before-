a081396ba83a896634b59adc3412ca705d72458e
CoconutL26 <2460045654@qq.com>
2023-10-17 23:50:20 +0800

    移动开发实验7

--- app/src/main/java/com/jnu/student/MyAdapter.java
+++ app/src/main/java/com/jnu/student/MyAdapter.java
@@ -21,6 +21,15 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         this.bookList = bookList;
     }
 
+    private int position;
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
     @Override
     public MyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.itemlayout, parent, false);
@@ -47,15 +56,16 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         public  void  onCreateContextMenu(ContextMenu menu,View v,
                                           ContextMenu.ContextMenuInfo menuInfo){
             menu.setHeaderTitle("具体操作");
-            menu.add(0,0, Menu.NONE,"添加");
-            menu.add(0,1, Menu.NONE,"删除");
-            menu.add(0,2, Menu.NONE,"修改");
+            menu.add(0,0, this.getAdapterPosition(),"添加");
+            menu.add(0,1, this.getAdapterPosition(),"删除");
+            menu.add(0,2, this.getAdapterPosition(),"修改");
         }
 
         public ViewHolder(View itemView) {
             super(itemView);
             bookCoverImageView = itemView.findViewById(R.id.image_view_book_cover);
             bookTitleTextView = itemView.findViewById(R.id.text_view_book_title);
+            itemView.setOnCreateContextMenuListener(this);
         }
 
 
